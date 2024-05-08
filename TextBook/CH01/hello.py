8f41b804cb6aaed97baec339494aac219950fa82
itc-n23019 <n23019@std.it-college.ac.jp>
2024-05-08 01:35:10 +0000

    Q1

---
+++ TextBook/CH01/hello.py
@@ -0,0 +1,10 @@
+print('Hello, world!')  # ②
+print('What is your name?')  # 名前を尋ねる
+my_name = input()  #3
+print('It is good to meet you, ' + my_name)  #4
+print('The length of your name is:')  # 名前の長さを表示
+print(len(my_name))
+print('What is your age?')  # 年齢を尋ねる
+my_age = input()
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')  # 来年の年齢を表示
+
